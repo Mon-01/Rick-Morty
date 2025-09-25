f5d70964234a6bb8f2035ad6191a06784716ee6e
Nieto Aranda <mnieto10@ttec.es>
2025-09-25 12:34:50 +0200

    página decoradas

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
 
-    fetch('navBar.html')
+    
+let nombre =  document.getElementById("navbar-container");
+if(nombre){
+       fetch('navBar.html')
       .then(response => response.text())
       .then(html => {
-        document.getElementById('navbar-container').innerHTML = html;
+        nombre.innerHTML = html;
       })
       .catch(error => console.error('Error cargando el navbar:', error));
-  
+}else{
+    fetch('navBarInicio.html')
+      .then(response => response.text())
+      .then(html => {
+        document.getElementById('navbar-container2').innerHTML = html;
+      })
+      .catch(error => console.error('Error cargando el navbar:', error));
+    }
+    
